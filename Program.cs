3baf89c4cf39e262c7ba0457b50db949bbefef01
AbhiprayIITP <40543067+AbhiprayIITP@users.noreply.github.com>
2020-08-16 14:57:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PythonSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
